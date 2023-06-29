1492031997e1ff3fac447f0d9ef8867e1f6ee17c
root <root@hyp-tst.us-central1-a.c.blockedge-381408.internal>
2023-06-29 10:40:52 +0000

    fixed chaincode

--- chaincode/basic/assetTransfer.go
+++ chaincode/basic/assetTransfer.go
@@ -40,30 +40,30 @@ type QueryResult struct {
 }
 
 // InitLedger adds a base set of cars to the ledger
-func (s *SmartContract) InitLedger(ctx contractapi.TransactionContextInterface) error {
-	assets := []Asset{
-		{ID: "asset1", Color: "blue", Size: 5, Owner: "Tomoko", AppraisedValue: 300},
-		{ID: "asset2", Color: "red", Size: 5, Owner: "Brad", AppraisedValue: 400},
-		{ID: "asset3", Color: "green", Size: 10, Owner: "Jin Soo", AppraisedValue: 500},
-		{ID: "asset4", Color: "yellow", Size: 10, Owner: "Max", AppraisedValue: 600},
-		{ID: "asset5", Color: "black", Size: 15, Owner: "Adriana", AppraisedValue: 700},
-		{ID: "asset6", Color: "white", Size: 15, Owner: "Michel", AppraisedValue: 800},
-	}
+// func (s *SmartContract) InitLedger(ctx contractapi.TransactionContextInterface) error {
+// 	assets := []Asset{
+// 		{ID: "asset1", Color: "blue", Size: 5, Owner: "Tomoko", AppraisedValue: 300},
+// 		{ID: "asset2", Color: "red", Size: 5, Owner: "Brad", AppraisedValue: 400},
+// 		{ID: "asset3", Color: "green", Size: 10, Owner: "Jin Soo", AppraisedValue: 500},
+// 		{ID: "asset4", Color: "yellow", Size: 10, Owner: "Max", AppraisedValue: 600},
+// 		{ID: "asset5", Color: "black", Size: 15, Owner: "Adriana", AppraisedValue: 700},
+// 		{ID: "asset6", Color: "white", Size: 15, Owner: "Michel", AppraisedValue: 800},
+// 	}
 
-	for _, asset := range assets {
-		assetJSON, err := json.Marshal(asset)
-		if err != nil {
-			return err
-		}
+// 	for _, asset := range assets {
+// 		assetJSON, err := json.Marshal(asset)
+// 		if err != nil {
+// 			return err
+// 		}
 
-		err = ctx.GetStub().PutState(asset.ID, assetJSON)
-		if err != nil {
-			return fmt.Errorf("failed to put to world state: %v", err)
-		}
-	}
+// 		err = ctx.GetStub().PutState(asset.ID, assetJSON)
+// 		if err != nil {
+// 			return fmt.Errorf("failed to put to world state: %v", err)
+// 		}
+// 	}
 
-	return nil
-}
+// 	return nil
+// }
 
 // CreateAsset issues a new asset to the world state with given details.
 func (s *SmartContract) CreateAsset(ctx contractapi.TransactionContextInterface, id, color string, size int, owner string, appraisedValue int) error {
